b43721e5d81aa2c55430bdcd5f9b3bf26c2f95ce
Shivani2001s <84129141+Shivani2001s@users.noreply.github.com>
2021-05-13 16:28:21 +0530

    Day 2 Assignment submitted

---
+++ Ascending.py
@@ -0,0 +1,6 @@
+a= ("Casio","Q&Q","Armani Exchange","Invicta","Titan")
+print(a)
+x=sorted(a)
+print("Ascending order -->")
+print(x)
+	
---
+++ overlapping.py
@@ -0,0 +1,7 @@
+list1 = [11,24,35,8,92,4,66,71]
+print("list 1 --> ",list1)
+list2 = [35,4,51,9,71,49,22,0]
+print("list 2 --> ",list2)
+list3 = set(list1).intersection(list2)
+print("Overlapping of list 1 and list 2 =>")
+print(list3)
